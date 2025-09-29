ab71bd863810dff0b8d00a0d1c1c967c1cd933be
carlos pinell <carlpine013@gmail.com>
2025-09-28 20:41:55 -0400

    todos los dockers para cada servicio

---
+++ bidding-service/src/test/java/SubastasMax/bidding_service/BiddingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package SubastasMax.bidding_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiddingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
